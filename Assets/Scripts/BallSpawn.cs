4fb4972e26346ccfbfeb2bc1d01bb727ff8a7619
DrAP <agus10pg@gmail.com>
2016-07-05 19:14:07 +0200

    Tutorial 3
    Spawn de pelotas

---
+++ Assets/Scripts/BallSpawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawn : MonoBehaviour {
+    float time;
+    public float spawnTime = 2f;
+    public GameObject ball;
+	// Use this for initialization
+	void Start () {
+        time = 100f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if(time>spawnTime)
+        {
+            Instantiate(ball, transform.position, transform.rotation);
+            time = 0f;
+        }
+	}
+}
