c7319abbe8323fa86c03288be5715908943c28d7
DrAP <agus10pg@gmail.com>
2016-07-13 13:05:12 +0200

    Tutorial 4
    Moviendo los personajes

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 10f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform transform = gameObject.GetComponent<Transform>();
+        Transform parentTransform = transform.parent.GetComponent<Transform>();
+        float x = Input.GetAxis("Horizontal");
+        Vector3 v;
+        v = parentTransform.right * speed * x * Time.deltaTime;
+        if ((Vector3.Scale(transform.position, parentTransform.right) + v).magnitude > 1.3f)
+            v = Vector3.Scale(transform.position, parentTransform.right) *
+                (1.3f - (Vector3.Scale(transform.position, Vector3.Scale(parentTransform.right, parentTransform.right))).magnitude);
+        transform.Translate(v, Space.World);
+	}
+}
